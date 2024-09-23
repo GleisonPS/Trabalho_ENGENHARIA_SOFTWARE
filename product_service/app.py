c62b70eae4f711cd5e71e4d64f40b36e1f70cac7
GleisonPS <gleisonp881@gmail.com>
2024-09-23 18:34:10 -0300

    Add Produce)Service

--- product_service/app.py
+++ product_service/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for, Blueprint
+from flask import Flask, jsonify, request
 import sqlite3
 
-product_service = Blueprint('product_service', __name__, template_folder='templates')
+app = Flask(__name__)
 
-@product_service.route('/')
-def index():
-    db = sqlite3.connect("product_service.sql")
-    
-    return "Página product_service"
+# Função para inicializar o banco de dados e criar a tabela 'products' se não existir
+def init_db():
+    conn = sqlite3.connect('products.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, price REAL)')
+    conn.commit()
+    conn.close()
+
+# Rota para obter todos os produtos
+@app.route('/products', methods=['GET'])
+def get_products():
+    conn = sqlite3.connect('products.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM products')
+    products = c.fetchall()
+    conn.close()
+    return jsonify(products)  # Retorna os produtos em formato JSON
+
+# Rota para adicionar um novo produto
+@app.route('/products', methods=['POST'])
+def add_product():
+    novo_produto = request.get_json()  # Obtém os dados do novo produto da requisição JSON
+    conn = sqlite3.connect('products.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO products (name, price) VALUES (?, ?)', (novo_produto['name'], novo_produto['price']))
+    conn.commit()
+    conn.close()
+    return jsonify(novo_produto), 201  # Retorna os dados do novo produto e status 201 (Criado)
+
+if __name__ == '__main__':
+    init_db()
+    app.run(port=5002)  # Executa a aplicação Flask na porta 5002
